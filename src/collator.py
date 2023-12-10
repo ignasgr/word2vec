fc7b153ca7d85164f4a11436aeba2ddd1953a697
Ignas G <ignasg.data@gmail.com>
2023-12-10 15:53:24 -0600

    added skipgram collator

--- src/collator.py
+++ src/collator.py
@@ -31,3 +31,34 @@ class CBOWCollator:
                 targets.append(target)
 
         return torch.tensor(contexts), torch.tensor(targets)
+
+
+class SkipGramCollator:
+    def __init__(self, context_length, vocab):
+        self.context_length = context_length
+        self.vocab = vocab
+
+    def collate(self, sentences):
+        """creates context, target pairs"""
+        contexts = []
+        targets = []
+
+        for sentence in sentences:
+            if len(sentence) < 1 + 2 * self.context_length:
+                continue
+
+            encoded = self.vocab.lookup_indices(sentence)
+
+            for target_idx in range(
+                self.context_length, len(encoded) - self.context_length
+            ):
+                target = encoded[target_idx]
+                context = (
+                    encoded[target_idx - self.context_length : target_idx]
+                    + encoded[target_idx + 1 : target_idx + self.context_length + 1]
+                )
+
+                contexts.extend(context)
+                targets.extend([target] * context)
+
+        return torch.tensor(context), torch.tensor(targets)
