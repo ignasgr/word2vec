3d060528ffed680d6cf8b66c8a6366afeae90369
Ignas G <ignasg.data@gmail.com>
2023-12-10 15:18:27 -0600

    added skipgram model

--- src/model.py
+++ src/model.py
@@ -11,3 +11,15 @@ class CBOW(nn.Module):
         embeds = self.embeddings(inputs).mean(dim=1)
         out = self.linear(embeds)
         return out
+
+
+class SkipGram(nn.Module):
+    def __init__(self, vocab_size, dims):
+        super().__init__()
+        self.embeddings = nn.Embedding(num_embeddings=vocab_size, embedding_dim=dims)
+        self.linear = nn.Linear(in_features=dims, out_features=vocab_size)
+
+    def forward(self, inputs):
+        embeds = self.embeddings(inputs)
+        out = self.linear(embeds)
+        return out
